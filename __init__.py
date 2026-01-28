b1fc6fac87d13085b74d9ecf07c70508281b7d8c
Enzo GIOIELLI <enzogio2004@gmail.com>
2026-01-28 17:12:39 +0100

    fix search

--- __init__.py
+++ __init__.py
@@ -139,7 +139,7 @@ def search_results():
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
         cursor.execute(
-            'SELECT * FROM clients WHERE titre LIKE ?',
+            'SELECT * FROM livres WHERE titre LIKE ?',
             ('%' + titre + '%',)
         )
         data = cursor.fetchall()
